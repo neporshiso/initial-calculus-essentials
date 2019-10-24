3e8d881237b7dd9d5b8a560bbcaae431be84554f
Daniel Farlow <dan.farlow@gmail.com>
2019-10-24 10:13:49 -0400

    modify seed data to correspond with new schema

--- seed.sql
+++ seed.sql
@@ -14,6 +14,6 @@ VALUES
     (DEFAULT, 'Logarithmic and Exponential Functions', 'logarithms');
 
 INSERT INTO problems
-    (id, problem_statement, problem_type, problem_answer, problem_solution, category_id)
+    (id, statement, type, answer_representation, answer_value, solution, category_id)
 VALUES
     (DEFAULT, 'sample problem statement', 'sample problem type (truefalse, manual_ordered, manual_unordered)', 'answer representation', 'simple answer to evaluate and compare', 'simple solution', 2);
