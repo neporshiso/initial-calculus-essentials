acfa7baef830040e4c2518f472c37a4d4a7c21af
Christopher Herrington <christopher.herrington@outlook.com>
2019-10-24 10:52:56 -0400

    modifies password field maxlen and makes email key uniquwe

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(30) NOT NULL
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE categories (
