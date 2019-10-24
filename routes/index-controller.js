88160ac8496dc9e1e964461bc3d0b772f13f6ab9
Christopher Herrington <christopher.herrington@outlook.com>
2019-10-24 10:53:42 -0400

    creates login/logout/signup routes and usermodel support

--- routes/index-controller.js
+++ routes/index-controller.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const indexModel = require('../models/index.model');
+const indexModel = require('../models/index-model');
 
 router.get('/', function(req, res, next) {
-  res.render('index-template', {
+  res.render('template', {
     locals: {
-      title: 'Welcome to the homepage!'
+      title: 'Welcome to the homepage!',
+      isLoggedIn: req.session.is_logged_in,
+      userName: req.session.username
+
     },
     partials: {
-      indexPartial: 'index-partial'
+      partial: 'partial-index'
     }
   });
 });
