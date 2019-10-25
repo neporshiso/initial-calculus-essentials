7364699485e7136dbfaf89db04ec4a196a70c103
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-24 23:08:34 -0400

    turned file storage back on because I need access to the user id in the router.post route in problem-controller. when user submits an answer, answer is now written into user_answers table. userAnswer model created

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-    // store: new FileStore(),
+    store: new FileStore(),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
--- routes/problem-controller.js
+++ routes/problem-controller.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const problemModel = require("../models/problem-model");
+const problemModel = require("../models/problem-model"),
+    userModel = require("../models/user-model"),
+    userAnswerModel = require("../models/userAnswer-model");
 
 router.get("/", async function(req, res, next) {
     const problemData = await problemModel.getProblemById(1);
@@ -21,20 +23,35 @@ router.get("/", async function(req, res, next) {
 
 router.post("/answerCheck", async (req, res, next) => {
     const { user_answer } = req.body;
+    const user_id = req.session.user_id;
 
     const problem = await problemModel.getProblemById(4),
         problem_answer = problem.answer_value,
         problem_type = problem.type;
-    
-    const evaluation = problemModel.answerCheck(problem_type, problem_answer, user_answer);
-    console.log(evaluation);
-    
+
+    const evaluation = problemModel.answerCheck(
+        problem_type,
+        problem_answer,
+        user_answer
+    );
+
+    const userAnswer = new userAnswerModel(
+        user_answer,
+        evaluation,
+        'TRUE',
+        user_id,
+        problem.id
+    );
+
+    const newAnswer = await userAnswer.createAnswer();
+    console.log(newAnswer);
+
     if (!!evaluation) {
+        console.log("Answer was correct");
         res.status(200).redirect("/problem");
     } else {
-        res.status(500).redirect("/problem")
+        res.status(500).redirect("/problem");
     }
-
 });
 
 module.exports = router;
