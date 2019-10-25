04ed9886f0784082e91a40ee0dff4db13e9ad6e6
Daniel Farlow <dan.farlow@gmail.com>
2019-10-25 17:55:10 -0400

    clean up problem view and header

--- models/problem-model.js
+++ models/problem-model.js
@@ -1,25 +1,5 @@
 const db = require("./conn");
 
-const sampleProblemStatement = String.raw`
-For $-\frac{\pi}{2}< x< \frac{\pi}{2}$, it follows that $\tan(x)\cos(x)=\sin(x)$.
-`;
-
-const sampleProblemType = "truefalse"; // manual_unordered truefalse
-
-const sampleProblemAnswerRepresentation = String.raw`
-True
-`;
-
-const sampleProblemAnswerValue = ["T"];
-
-const sampleProblemSolution = String.raw`
-Since $\tan(x)=\frac{\sin(x)}{\cos(x)}$ and $\cos(x)\neq0$ when $x\in(-\pi/2,\pi/2)$, we have the following:
-$$
-\tan(x)\cos(x)=\frac{\sin(x)}{\cos(x)}\cos(x)=\sin(x).
-$$
-`;
-const sampleProblemCategoryId = 3;
-
 class Problem {
     constructor(
         id,
@@ -38,7 +18,6 @@ class Problem {
         this.solution = solution;
         this.category_id = category_id;
     }
-
 
     static base64Decode (str, encoding = 'utf-8') {
         return Buffer.from(str, 'base64').toString('utf8')
@@ -74,16 +53,6 @@ class Problem {
     static async getAll() {
         try {
             const response = await db.query(`SELECT * FROM problems;`);
-
-            // console.log(response);
-
-            // for (let problem of response) {
-            //     problem.problem_statement = Base64Decode(problem.problem_statement);
-            // }
-
-            // const decodedResponses = response.map(problem => Base64Decode(problem.problem_statement))
-            // return decodedResponses;
-
             return response;
         } catch (error) {
             console.log(error.message);
